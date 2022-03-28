1bf4442de8a4660d93b7beb5f1999e1b7b7e1cfc
wmbat <wmbat-dev@protonmail.com>
2022-03-27 20:26:02 -0400

    Gitsigns

--- lua/wmbat-dark.lua
+++ lua/wmbat-dark.lua
@@ -320,10 +320,20 @@ Group.new('illuminatedWork', nil, nil, styles.bold)
 
 Group.new('TelescopeMatching', colors.green, nil, styles.bold)
 Group.new('TelescopeBorder', groups.Purple, groups.Purple, groups.Purple)
--- Group.new('TelescopePromptPrefix' groups.Blue, groups.Blue, group.Blue)
--- Group.new('TelescopeSelection', groups.DiffAdd, groups.DiffAdd, groups.DiffAdd)
+Group.new('TelescopePromptPrefix' groups.Blue, groups.Blue, groups.Blue)
+Group.new('TelescopeSelection', groups.DiffAdd, groups.DiffAdd, groups.DiffAdd)
 
--- lewis6991/gitsigns.nvim
+-- lewis6991/gitsigns.nvim-tree
+Group.new('GitSignsAdd', groups.GreenSign, groups.GreenSign, groups.GreenSign)
+Group.new('GitSignsChange', groups.BlueSign, groups.BlueSign, groups.BlueSign)
+Group.new('GitSignsDelete', groups.RedSign, groups.RedSign, groups.RedSign)
+Group.new('GitSignsAddNr', groups.Green, groups.Green, groups.Green)
+Group.new('GitSignsChangeNr', groups.Blue, groups.Blue, groups.Blue)
+Group.new('GitSignsDeleteNr', groups.Red, groups.Red, groups.Red)
+Group.new('GitSignsAddLn', groups.DiffAdd, groups.DiffAdd, groups.DiffAdd)
+Group.new('GitSignsChangeLn', groups.DiffChange, groups.DiffChange, groups.DiffChange)
+Group.new('GitSignsDeleteLn', groups.DiffDelete, groups.DiffDelete, groups.DiffDelete)
+Group.new('GitSignsCurrentLineBlame', groups.Grey, groups.Grey, groups.Grey)
 
 -- p00f/nvim-ts-rainbow
 
