21a099c15bade68cb072c3f035489262c2c7387c
wmbat <wmbat-dev@protonmail.com>
2022-04-24 13:20:48 -0400

    debugging 8

--- lua/wmbat-dark/plugins/treesitter.lua
+++ lua/wmbat-dark/plugins/treesitter.lua
@@ -12,7 +12,7 @@ vim.pretty_print(table_concat)
 local group = lush(function()
 	return {
 		TSNone {},
-		TSError { wmbat_dark_error },
+--		TSError { wmbat_dark_error },
 		TSStrong { gui = styles.bold },
 		TSEmphasis { gui = styles.italic},
 		TSUnderline { gui = styles.underline },
