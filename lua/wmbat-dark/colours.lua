b962731dc552964706b679c998e64c2e04b5ca9f
wmbat <wmbat-dev@protonmail.com>
2022-04-24 18:43:28 -0400

    gitsigns 8

--- lua/wmbat-dark/colours.lua
+++ lua/wmbat-dark/colours.lua
@@ -2,7 +2,7 @@ local hsl = require('lush.hsl')
 
 return {
 	white = hsl('#c5cdd9'),
-	black_0 = hsl('#202023'),
+	black_0 = hsl('#2b2d3a'),
 	black_1 = hsl('#333648'),
 	black_2 = hsl('#363a4e'),
 	black_3 = hsl('#393e53'),
--- lua/wmbat-dark/init.lua
+++ lua/wmbat-dark/init.lua
@@ -5,18 +5,11 @@ local config_module = require("wmbat-dark.config")
 local config = config_module.config
 
 local function setup(user_config)
-	vim.pretty_print("setup");
-
 	if user_config then
 		config_module.apply_config(user_config)
 	end
 
-	vim.pretty_print("loading plugins")
-
-	local load_plugins = loader.load_plugins(config)		
-
-	vim.pretty_print("loading langs")
-
+	local load_plugins = loader.load_plugins(config)
 	local load_langs = loader.load_langs(config)
 
 	return lush.merge({base, load_plugins, load_langs})
