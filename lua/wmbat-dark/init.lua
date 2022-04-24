7b9574ad57a4274ed7a325a2b9f7b10c088ce164
wmbat <wmbat-dev@protonmail.com>
2022-04-24 18:01:48 -0400

    treesitter 1

--- lua/wmbat-dark/init.lua
+++ lua/wmbat-dark/init.lua
@@ -5,23 +5,12 @@ local config_module = require("wmbat-dark.config")
 local config = config_module.config
 
 local function setup(user_config)
-
-	vim.pretty_print(user_config)
-	vim.pretty_print(config_module)
-
 	if user_config then
 		config_module.apply_config(user_config)
 	end
 
-	vim.pretty_print(config)
-
-	local load_plugins = loader.load_plugins(config)
-		
-	vim.pretty_print(load_plugins)
-
+	local load_plugins = loader.load_plugins(config)		
 	local load_langs = loader.load_langs(config)
-
-	vim.pretty_print(load_langs)
 
 	return lush.merge({base, load_plugins, load_langs})
 end
--- lua/wmbat-dark/utils/loader.lua
+++ lua/wmbat-dark/utils/loader.lua
@@ -6,30 +6,15 @@ local function load_plugins(config)
 	vim.pretty_print('loading plugins...')
 
 	for index, plugin in pairs(config.plugins) do
-		
-		vim.pretty_print(plugin)
-
 		if type(plugin) == 'string' then
-			local plugin_path = 'wmbat-dark.plugins.' .. plugin
-
-			vim.pretty_print(plugin_path)
-
-			local local_plugin = require(plugin_path)
-			
-			vim.pretty_print(local_plugin)
-
+			local local_plugin = require('wmbat-dark.plugins.' .. plugin)	
 			config.plugins[index] = local_plugin
 		elseif type(plugin) == 'table' then
-			vim.pretty_print('table path')
-
 			config.plugins[index] = plugin
 		end
 	end
 
 	local loaded_plugins = lush.merge(config.plugins)
-
-	vim.pretty_print(loaded_plugins)
-
 	return loaded_plugins
 end
 
@@ -39,18 +24,13 @@ local function load_langs(config)
 	for index, lang in pairs(config.langs) do
 		if type(lang) == 'string' then
 			local local_lang = require('wmbat-dark.languages.' .. lang)
-
-			vim.pretty_print(local_lang)
-
 			config.langs[index] = local_lang
 		elseif type(lang) == 'table' then
 			config.langs[index] = lang
 		end
 	end
+
 	local loaded_langs = lush.merge(config.langs)
-
-	vim.pretty_print(loaded_langs)
-
 	return loaded_langs
 end
 
