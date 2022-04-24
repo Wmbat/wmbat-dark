953d5a7875dd82b9409ab59e21b621a78bd73e0e
wmbat <wmbat-dev@protonmail.com>
2022-04-24 11:17:20 -0400

    Debugging prints

--- lua/wmbat-dark/init.lua
+++ lua/wmbat-dark/init.lua
@@ -5,12 +5,21 @@ local config_module = require("wmbat-dark.config")
 local config = config_module.config
 
 local function setup(user_config)
+
+	vim.pretty_print(user_config)
+
 	if user_config then
 		config_module.apply_config(user_config)
 	end
 
 	local load_plugins = loader.load_plugins(config)
+		
+	vim.pretty_print(load_plugins)
+
 	local load_langs = loader.load_langs(config)
+
+	vim.pretty_print(load_langs)
+
 	return lush.merge({base, load_plugins, load_langs})
 end
 
