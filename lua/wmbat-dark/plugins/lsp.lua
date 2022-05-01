faa6f93217dbe0c3bee554ed9c18706f5526c221
wmbat <wmbat-dev@protonmail.com>
2022-05-01 13:39:52 -0400

    LSP plugin highlight again

--- lua/wmbat-dark/plugins/lsp.lua
+++ lua/wmbat-dark/plugins/lsp.lua
@@ -8,29 +8,14 @@ local table_concat = table.concat
 local group = lush(function()
 	return {
 		LspDiagnosticsDefaultError { fg = colours.red }, 
-		LspDiagnosticsDefaultWarn { fg = colours.yellow }, 
-		LspDiagnosticsDefaultInfo { fg = colours.blue },
+		LspDiagnosticsDefaultWarning { fg = colours.yellow }, 
+		LspDiagnosticsDefaultInformation { fg = colours.blue },
 		LspDiagnosticsDefaultHint { fg = colours.green },
 
-		LspDiagnosticsFloatingError { fg = colours.red },
-		LspDiagnosticsFloatingWarn { fg = colours.yellow },
-		LspDiagnosticsFloatingInfo { fg = colours.blue },
-		LspDiagnosticsFloatingHint { fg = colours.green },
-
-		LspDiagnosticsVirtualTextError { fg = colours.red },
-		LspDiagnosticsVirtualTextWarn { fg = colours.yellow },
-		LspDiagnosticsVirtualTextInfo { fg = colours.blue },
-		LspDiagnosticsVirtualTextHint { fg = colours.green },
-
 		LspDiagnosticsUnderlineError { fg = colours.red, gui = styles.undercurl },
-		LspDiagnosticsUnderlineWarn { fg = colours.yellow, gui = styles.undercurl },
-		LspDiagnosticsUnderlineInfo { fg = colours.blue, gui = styles.undercurl },
+		LspDiagnosticsUnderlineWarning { fg = colours.yellow, gui = styles.undercurl },
+		LspDiagnosticsUnderlineInformation { fg = colours.blue, gui = styles.undercurl },
 		LspDiagnosticsUnderlineHint { fg = colours.green, gui = styles.undercurl },
-
-		LspDiagnosticsSignError { fg = colours.red },
-		LspDiagnosticsSignWarn { fg = colours.yellow },
-		LspDiagnosticsSignInfo { fg = colours.blue },
-		LspDiagnosticsSignHint { fg = colours.green },
 
 		LspReferenceText { gui = styles.bold },
 		LspReferenceRead { gui = styles.bold },
