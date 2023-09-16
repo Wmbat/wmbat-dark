88e4b9679c6dd9caae99bdad156d45cdf444dd3d
wmbat <wmbat-dev@protonmail.com>
2023-09-16 12:30:29 -0400

    Added highlight for the LSP active parameter

--- lua/wmbat-dark/init.lua
+++ lua/wmbat-dark/init.lua
@@ -193,7 +193,7 @@ local theme = lush(function(injected_functions)
 		LspReferenceWrite { gui = styles.bold }, -- Used for highlighting "write" references
 		-- LspCodeLens                 { } , -- Used to color the virtual text of the codelens. See |nvim_buf_set_extmark()|.
 		-- LspCodeLensSeparator        { } , -- Used to color the seperator between two or more code lens.
-		-- LspSignatureActiveParameter { } , -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
+		LspSignatureActiveParameter { gui = styles.invert_selection } , -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
 
 		TextError { fg = colours.red },
 		TextWarning { fg = colours.orange },
