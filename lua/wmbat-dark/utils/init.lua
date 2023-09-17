f67481b6945bbbec85ed9fd82482d108b4ae4341
wmbat <wmbat-dev@protonmail.com>
2023-09-17 17:49:11 -0400

    Cleaning up utils

--- lua/wmbat-dark/utils/init.lua
+++ lua/wmbat-dark/utils/init.lua
@@ -1,24 +1,26 @@
+-- util functions
 local M = {}
 
 M.tobool = function(val)
-	if val == 0 or not val then
-		return false
-	end
-	return true
+    if val == 0 or not val then
+        return false
+    end
+    return true
 end
 
-M.get_colour_from_var = function(colour, default, colours)
-	if colour == nil then
-		return default
-	end
-	
-	local c = colours[colour]
-	if c == nil then
-		print(string.format("%s colour could not be found, using default", colour))
-		return default
-	end
+-- check if vim.g.wmbat-dark_* color exists in current palette, return default color
+-- otherwise
+M.get_color_from_var = function(color, default, colors)
+    if color == nil then
+        return default
+    end
 
-	return c
+    local c = colors[color]
+    if c == nil then
+        print(string.format("%s color could not be found, using default", color))
+        return default
+    end
+    return c
 end
 
 return M
