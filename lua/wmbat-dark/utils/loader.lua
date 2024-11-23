ff4c75bc982074dace92c061808b59dc7dbecc5c
TheWmbat <ambre.spehner@protonmail.com>
2024-11-23 06:46:10 -0500

    Improved debug printing

--- lua/wmbat-dark/utils/loader.lua
+++ lua/wmbat-dark/utils/loader.lua
@@ -5,9 +5,10 @@ local lush = require("lush")
 local function load_plugins(config)
     for index, plugin in pairs(config.plugins) do
         if type(plugin) == 'string' then
-            local local_plugin = require('wmbat-dark.plugins.' .. plugin)
 
-            vim.print(local_plugin)
+            vim.print('wmbat-dark.plugins' .. plugin)
+
+            local local_plugin = require('wmbat-dark.plugins.' .. plugin)
             config.plugins[index] = local_plugin
         elseif type(plugin) == 'table' then
             config.plugins[index] = plugin
