ab798e355c71718d0fb2c1c4642c69fc718a3dcf
TheWmbat <ambre.spehner@protonmail.com>
2024-11-23 07:04:47 -0500

    EVEN MORE PLUGINS !!!!

--- lua/wmbat-dark/plugins/treesitter.lua
+++ lua/wmbat-dark/plugins/treesitter.lua
@@ -1,9 +1,7 @@
 -- treesitter highlights
 local lush = require("lush")
-local base = require("wmbat-dark.base")
 local styles = require("wmbat-dark.settings").styles
 local colours = require("wmbat-dark.colours")
-local table_concat = table.concat
 
 local group = lush(function()
     return {
