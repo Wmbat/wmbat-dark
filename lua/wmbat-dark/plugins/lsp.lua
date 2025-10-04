6a16eb2e63afc4e921efb489d38f832d3c657675
TheWmbat <ambre.spehner@protonmail.com>
2025-10-04 16:59:08 -0400

    Inlay hint 6

--- lua/wmbat-dark/plugins/lsp.lua
+++ lua/wmbat-dark/plugins/lsp.lua
@@ -71,7 +71,7 @@ local group = lush(function()
         LspReferenceRead { gui = styles.bold },
         LspReferenceWrite { gui = styles.bold },
 
-        LspInlayHint { bg = colours.black_2, gui = styles.bold },
+        LspInlayHint { fg = colours.black_2, gui = styles.bold },
 
         ["@lsp.type.namespace"] = { fg = colours.red },
         ["@lsp.type.type"] = { fg = colours.yellow },
